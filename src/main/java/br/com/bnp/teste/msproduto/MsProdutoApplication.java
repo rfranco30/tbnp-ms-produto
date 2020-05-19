02b3c657fcf68f6da599cd228cc0cbb7c5996602
Unknown <rodrigofranco30@gmail.com>
2020-05-19 19:44:01 -0300

    eureka

--- src/main/java/br/com/bnp/teste/msproduto/MsProdutoApplication.java
+++ src/main/java/br/com/bnp/teste/msproduto/MsProdutoApplication.java
@@ -3,8 +3,10 @@ package br.com.bnp.teste.msproduto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @EnableJpaRepositories
 @SpringBootApplication
